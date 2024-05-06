d99d04b52856503445aa7ce5883dbc6ac09dc040
Trung <ledinhduytrungcons@gmail.com>
2024-05-06 20:16:19 +0700

    Refactor Project Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,94 +1,10 @@
-import { useColorScheme } from '@mui/material/styles'
-import InputLabel from '@mui/material/InputLabel'
-import MenuItem from '@mui/material/MenuItem'
-import FormControl from '@mui/material/FormControl'
-import Select from '@mui/material/Select'
-import LightModeIcon from '@mui/icons-material/LightMode'
-import DarkModeIcon from '@mui/icons-material/DarkMode'
-import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
-import Box from '@mui/material/Box'
-import Container from '@mui/material/Container'
-
-function ModeSelect() {
-  const { mode, setMode } = useColorScheme()
-
-  const handleChange = (event) => {
-    // const selectedMode = event.target.value
-    // setMode(selectedMode)
-    setMode(event.target.value)
-  }
-
-  return (
-    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
-      <Select
-        labelId="label-select-dark-light-mode"
-        id="select-dark-light-mode"
-        value={mode}
-        label="Mode"
-        onChange={handleChange}
-      >
-        <MenuItem value="light">
-          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-            <LightModeIcon fontSize="small" />
-            Light
-          </Box>
-        </MenuItem>
-        <MenuItem value="dark">
-          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-            <DarkModeIcon fontSize="small" />
-            Dark
-          </Box>
-        </MenuItem>
-        <MenuItem value="system">
-          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-            <SettingsBrightnessIcon fontSize="small" />
-            System
-          </Box>
-        </MenuItem>
-      </Select>
-    </FormControl>
-  )
-}
+import Board from './pages/Boards/_id'
 
 function App() {
   return (
-    <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
-      <Box
-        sx={{
-          width: '100%',
-          height: (theme) => theme.trelloCustom.appBarHeight,
-          display: 'flex',
-          alignItems: 'center',
-          backgroundColor: 'primary.light'
-        }}
-      >
-        <ModeSelect />
-      </Box>
-      <Box
-        sx={{
-          width: '100%',
-          height: (theme) => theme.trelloCustom.boardBarHeight,
-          display: 'flex',
-          alignItems: 'center',
-          backgroundColor: 'primary.dark'
-        }}
-      >
-        Board Bar
-      </Box>
-      <Box
-        sx={{
-          width: '100%',
-          height: (theme) =>
-            `calc(100vh - ${theme.trelloCustom.appBarHeight} - ${theme.trelloCustom.boardBarHeight})`,
-          display: 'flex',
-          alignItems: 'center',
-          backgroundColor: 'primary.main'
-        }}
-      >
-        Board Content
-      </Box>
-    </Container>
+    <>
+      <Board />
+    </>
   )
 }
 
